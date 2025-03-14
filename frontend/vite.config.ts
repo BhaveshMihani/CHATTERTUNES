7ae48e19081992e09a26d584c44e03e6faccfcfa
Bhavesh Mihani <bhaveshmihani07@gmail.com>
2025-03-14 17:05:34 +0530

    Auth Functionality Upgraded!!

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,25 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
 export default defineConfig({
-	plugins: [react()],
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
-		},
-	},
-	server: {
-		port: 3000,
-	},
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss,
+        autoprefixer,
+      ],
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    port: 3000,
+  },
 });
